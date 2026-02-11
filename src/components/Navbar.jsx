5304831128f24d48134fbfa239bcfff758baebfc
Mrigank Mouli Singh <mriganksinghofficial@gmail.com>
2026-02-11 15:32:48 +0530

    optimized the white pill size

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -66,7 +66,7 @@ export default function Navbar() {
                 ref={(el) => (itemsRef.current[index] = el)}
                 // INCREASED PADDING (px-10) here makes the pill wider on both sides
                 className={({ isActive }) =>
-                  `relative z-10 px-8 py-2 font-medium text-[24px] transition-colors duration-300 whitespace-nowrap ${
+                  `relative z-10 px-4 py-2 font-medium text-[24px] transition-colors duration-300 whitespace-nowrap ${
                     isActive ? "text-[#0047AB]" : "text-white hover:text-blue-100"
                   }`
                 }
