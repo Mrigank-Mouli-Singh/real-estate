2d70f42523c07c7887aad7c67036124bbd059191
yagyasoni <yagyasony@gmail.com>
2026-02-16 17:12:22 +0530

    services code merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 import Service from "./Pages/Service.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
     <>
-      <Service/>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div className="text-center py-20">
+              Welcome to the Real Estate App
+            </div>
+          }
+        />
+        <Route path="/services" element={<Service />} />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
